e22eeaa4ca44d587d35cc6932614b3af0964606c
aracelicaballero <acaballe@iespompeufabra.cat>
2026-09-23 14:19:31 +0200

    Create sketch.js

---
+++ cara1/sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(600, 600);
+}
+
+function draw() {
+  background(215, 126, 217);
+  ellipse(300, 300, 200, 250)
+  ellipse(260, 270, 50, 30)
+  ellipse(340, 270, 50, 30)
+  arc(300, 350, 99, 40, 0, PI)
+  triangle(300, 300, 330, 360, )
+  
+}
